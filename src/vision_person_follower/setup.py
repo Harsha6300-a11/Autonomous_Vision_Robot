96387bf60666a452c1107ce73df1e89a26d70f21
Harsha Chintagunta <harshachintagunta@gmail.com>
2026-02-01 10:08:52 +0100

    Update setup.py

--- src/vision_person_follower/setup.py
+++ src/vision_person_follower/setup.py
@@ -7,18 +7,26 @@ setup(
     version='0.0.1',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch',
-            ['launch/empty_world_follow.launch.py']),
+        (
+            'share/ament_index/resource_index/packages',
+            ['resource/' + package_name]
+        ),
+        (
+            'share/' + package_name,
+            ['package.xml']
+        ),
+        (
+            'share/' + package_name + '/launch',
+            ['launch/vision_person_follower.launch.py']
+        ),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Harsha',
-    maintainer_email='harshachintagunta@gmail.com',
-    description='Vision-based person follower using YOLOv8, DeepSORT, and LiDAR',
+    maintainer_email='harsha@example.com',
+    description='ROS 2 vision-based person follower using YOLOv8, DeepSORT, and LiDAR',
     license='Apache License 2.0',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'vision_follower = vision_person_follower.controller_node:main',
